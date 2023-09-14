eaa704e97e2b352b5128aaff56f15b2bad83c015
joseph-banaag <josephrbanaag51@gmail.com>
2023-09-14 14:35:55 +0800

    added identifier for the theme toggle switch

--- components/toggle/SwitchToggle.jsx
+++ components/toggle/SwitchToggle.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import { MoonIcon } from "../utils/icons/MoonIcon";
 import { SunIcon } from "../utils/icons/SunIcon";
 import { MySwitch } from "../utils/tailwindvariants/tv";
-import { Tooltip } from "@nextui-org/react";
 
 export default function SwitchToggle({ onChange, themes }) {
   const [clicked, setClicked] = useState(false);
@@ -22,12 +21,7 @@ export default function SwitchToggle({ onChange, themes }) {
         startContent={<SunIcon />}
         endContent={<MoonIcon />}
         onChange={handleClick}
-        className="group"
-      >
-        <span class="group-hover:opacity-100 transition-opacity bg-[#C9A9E9] py-1 px-2 text-xs text-black/80 rounded-md absolute left-0 top-0 opacity-0 translate-x-1 -translate-y-3 mx-auto">
-          <span className="font-semibold">Theme:</span> {themes}
-        </span>
-      </MySwitch>
+      ></MySwitch>
     </>
   );
 }
