68a3a62df31de9081f6ccbb745b8d63d689819cc
joseph-banaag <josephrbanaag51@gmail.com>
2023-09-21 19:48:45 +0800

    added an alert component if the account does not exist and redirect user to sign up

--- app/(root)/(forms)/sign-in/page.tsx
+++ app/(root)/(forms)/sign-in/page.tsx
@@ -12,7 +12,8 @@ import { useRouter } from 'next/navigation'
 import SocialAuth from "@/components/utils/SocialAuth";
 import { Db_userInformation_from_SI } from "@/app/(user)/dashboard/userDB_info/UserInfo_DB";
 import SubmitSpinner from "@/components/lib/SubmitSpinner";
-import SignInErrors from "@/components/(..)modals/alerts/SignInErrors";
+import NoAccount from "@/components/utils/warnings/alerts/NoAccount";
+import { client } from "@/app/lib/mongodb";
 
 // this object is for type declaration of useForm() function specifically for register method.
 interface Inputs {
@@ -35,6 +36,7 @@ async function getData() {
 export default function SignIn() {
     const [isVisible, setIsVisible] = React.useState(false);
     const [clicked, setClicked] = React.useState(false)
+    const [noAccount, setNoAccount] = React.useState(false)
     const router = useRouter()
 
 
@@ -71,27 +73,14 @@ export default function SignIn() {
             const passwordInput = password
             const usernameInput = user_name
 
-            // console.log(data_from_DB.length)
-
             const userInfo_DB = data_from_DB.find((obj: { username: any; }) => obj.username === usernameInput)
 
-            // console.log(userInfo_DB)
-
             if (userInfo_DB === undefined) {
-                alert("Please sign up to create an account")
-                // TODO: create a modal for the error message and add the router to the close button
+                // no existing account
+                setNoAccount(!noAccount)
                 setClicked(!clicked)
-
-                setTimeout(() => {
-                    router.push("/sign-up")
-                }, 1000);
-                
-                return (
-                    <>
-                        < SignInErrors />
-                    </>
-                )
             } else {
+                // with an existing account
                 const db_username = userInfo_DB.username
                 const db_email = userInfo_DB.email
                 const db_password = userInfo_DB.password
@@ -124,117 +113,122 @@ export default function SignIn() {
     }
 
     return (
-        <> <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ ease: "backIn", duration: 0.25 }}
-        >
-            <div className=" w-full min-h-screen flex flex-1 flex-col justify-center items-center">
-                <div className="sm:p-5 p-3">
-                    <Card className="flex flex-col flex-1 rounded-2xl p-5 gap-5 mb-24 shadow-2xl max-w-[640px] bg-background/60 dark:bg-default-100/50" id="signOptions">
-
-                        <SocialAuth />
-
-                        <div className="flex flex-1 justify-center items-center">
-                            <hr className='w-full'></hr>
-                            <p className="sm:px-3 p-1 sm:text-medium text-xs sm:font-normal font-small">or</p>
-                            <hr className='w-full'></hr>
-                        </div>
-
-                        {/* form */}
-                        <form onSubmit={handleSubmit(OnSubmit)} className='flex flex-col gap-3'>
-                            <div className='flex flex-col'>
-                                <Input
-                                    autoComplete="off"
-                                    aria-autocomplete="none"
-                                    id="username"
-                                    type="text"
-                                    isClearable
-                                    variant="bordered"
-                                    label="Username"
-                                    className="w-full flex-1 transition-all duration-300"
-                                    classNames={{
-                                        label: "text-black/50 dark:text-white/90 sm:text-sm text-xs sm:font-normal font-small ",
-                                        input: [
-                                            "sm:text-medium text-sm sm:font-normal font-normal",
-                                            "bg-transparent",
-                                            "text-black/90 dark:text-white/90",
-                                            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-                                        ],
-                                    }}
-                                    {...register("username", {
-                                        required: true
-                                    })}
-                                />
-
-                                <p className="animate-pulse text-xs text-red-400">
-                                    {errors.username?.types?.required && <span>Username is required</span>}
-                                </p>
-                            </div>
-                            <div className='flex flex-col'>
-                                <div className="flex justify-end">
-                                    <PasswordReset />
-                                </div>
-                                <Input
-                                    autoComplete="off"
-                                    aria-autocomplete="none"
-                                    id="password"
-                                    variant="bordered"
-                                    label="Password"
-                                    classNames={{
-                                        label: "text-black/50 dark:text-white/90 sm:text-sm text-xs sm:font-normal font-small ",
-                                        input: [
-                                            "sm:text-medium text-sm sm:font-normal font-normal",
-                                            "bg-transparent",
-                                            "text-black/90 dark:text-white/90",
-                                            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-                                        ],
-                                    }}
-                                    endContent={
-                                        <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
-                                            {isVisible ? (
-                                                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                                            ) : (
-                                                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                                            )}
-                                        </button>
-                                    }
-                                    type={isVisible ? "text" : "password"}
-                                    className="w-full flex-1"
-                                    {...register("password", {
-                                        required: true
-                                    })}
-                                    name="password"
-                                />
-
-                                <p className="animate-pulse text-xs text-red-400">
-                                    {errors.password?.types?.required && <span>Password is required</span>}
-                                </p>
-                            </div>
-                            <div className='flex flex-col gap-1 my-3'>
-                                <Button type="submit" name="submit" className="bg-green-800 hover:bg-green-950 drop-shadow-lg transition-all duration-300">
-                                    <p className="text-slate-300 hover:text-white font-semibold flex-1">
-                                        {clicked
-                                            ? <SubmitSpinner />
-                                            : "Continue"
-                                        }</p>
-                                </Button>
-                            </div>
-                        </form>
-                    </Card>
+        <>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ ease: "backIn", duration: 0.25 }}
+            >
+                <div className={`${noAccount ? "block" : "hidden"} `}>
+                    <NoAccount />
                 </div>
-            </div>
 
-            <div className="flex md:flex-row flex-col justify-center items-center p-10 z-0 mb-10 md:gap-11 gap-5">
-                <h1 className="sm:text-2xl text-medium font-bold text-[#FB542B] drop-shadow-xl">Connect with the community</h1>
-                <Image
-                    src={bgIllustration.group.src}
-                    alt={bgIllustration.group.name}
-                    width={400}
-                    height={400}
-                />
-            </div>
-        </motion.div>
+                <div className=" w-full min-h-screen flex flex-1 flex-col justify-center items-center">
+                    <div className="sm:p-5 p-3">
+                        <Card className="flex flex-col flex-1 rounded-2xl p-5 gap-5 mb-24 shadow-2xl max-w-[640px] bg-background/60 dark:bg-default-100/50" id="signOptions">
+
+                            <SocialAuth />
+
+                            <div className="flex flex-1 justify-center items-center">
+                                <hr className='w-full'></hr>
+                                <p className="sm:px-3 p-1 sm:text-medium text-xs sm:font-normal font-small">or</p>
+                                <hr className='w-full'></hr>
+                            </div>
+
+                            {/* form */}
+                            <form onSubmit={handleSubmit(OnSubmit)} className='flex flex-col gap-3'>
+                                <div className='flex flex-col'>
+                                    <Input
+                                        autoComplete="off"
+                                        aria-autocomplete="none"
+                                        id="username"
+                                        type="text"
+                                        isClearable
+                                        variant="bordered"
+                                        label="Username"
+                                        className="w-full flex-1 transition-all duration-300"
+                                        classNames={{
+                                            label: "text-black/50 dark:text-white/90 sm:text-sm text-xs sm:font-normal font-small ",
+                                            input: [
+                                                "sm:text-medium text-sm sm:font-normal font-normal",
+                                                "bg-transparent",
+                                                "text-black/90 dark:text-white/90",
+                                                "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+                                            ],
+                                        }}
+                                        {...register("username", {
+                                            required: true
+                                        })}
+                                    />
+
+                                    <p className="animate-pulse text-xs text-red-400">
+                                        {errors.username?.types?.required && <span>Username is required</span>}
+                                    </p>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <div className="flex justify-end">
+                                        <PasswordReset />
+                                    </div>
+                                    <Input
+                                        autoComplete="off"
+                                        aria-autocomplete="none"
+                                        id="password"
+                                        variant="bordered"
+                                        label="Password"
+                                        classNames={{
+                                            label: "text-black/50 dark:text-white/90 sm:text-sm text-xs sm:font-normal font-small ",
+                                            input: [
+                                                "sm:text-medium text-sm sm:font-normal font-normal",
+                                                "bg-transparent",
+                                                "text-black/90 dark:text-white/90",
+                                                "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+                                            ],
+                                        }}
+                                        endContent={
+                                            <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                                                {isVisible ? (
+                                                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                                ) : (
+                                                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                                )}
+                                            </button>
+                                        }
+                                        type={isVisible ? "text" : "password"}
+                                        className="w-full flex-1"
+                                        {...register("password", {
+                                            required: true
+                                        })}
+                                        name="password"
+                                    />
+
+                                    <p className="animate-pulse text-xs text-red-400">
+                                        {errors.password?.types?.required && <span>Password is required</span>}
+                                    </p>
+                                </div>
+                                <div className='flex flex-col gap-1 my-3'>
+                                    <Button type="submit" name="submit" className="bg-green-800 hover:bg-green-950 drop-shadow-lg transition-all duration-300">
+                                        <p className="text-slate-300 hover:text-white font-semibold flex-1">
+                                            {clicked
+                                                ? <SubmitSpinner />
+                                                : "Continue"
+                                            }</p>
+                                    </Button>
+                                </div>
+                            </form>
+                        </Card>
+                    </div>
+                </div>
+
+                <div className="flex md:flex-row flex-col justify-center items-center p-10 z-0 mb-10 md:gap-11 gap-5">
+                    <h1 className="sm:text-2xl text-medium font-bold text-[#FB542B] drop-shadow-xl">Connect with the community</h1>
+                    <Image
+                        src={bgIllustration.group.src}
+                        alt={bgIllustration.group.name}
+                        width={400}
+                        height={400}
+                    />
+                </div>
+            </motion.div>
         </>
     )
 }
---
+++ components/utils/warnings/alerts/NoAccount.tsx
@@ -0,0 +1,68 @@
+"use client"
+import React from "react";
+import { Card, CardBody, Button, CardFooter } from "@nextui-org/react";
+import { motion } from "framer-motion"
+import SubmitSpinner from "@/components/lib/SubmitSpinner";
+import { useRouter } from 'next/navigation'
+
+
+export default function NoAccount() {
+    const [clicked, setClicked] = React.useState(false)
+    const router = useRouter()
+
+    const handleClick = () => {
+        const newClick = !clicked;
+        setClicked(newClick)
+
+        setTimeout(() => {
+            router.push("/sign-up")
+        }, 1000);
+    }
+
+    return (
+        <>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ ease: "backIn", duration: 0.25 }}
+            >
+                <div className="p-3 fixed z-50 top-[20%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full">
+                    <Card
+                        className="
+                    border-none 
+                    bg-background/80 
+                    dark:bg-default-100/90 
+                    max-w-[620px]
+                    mx-auto
+                    flex-1
+                    shadow-2xl
+                    "
+                        shadow="sm"
+                    >
+                        <CardBody>
+                            <p className="sm:text-medium text-xs sm:font-normal font-small drop-shadow-md tracking-wide">
+                                Please sign up to create an account...
+                            </p>
+                        </CardBody>
+                        <CardFooter className="flex flex-row-reverse">
+                            <Button
+                                onClick={handleClick}
+                                color="success"
+                                size="sm"
+                                className="bg-green-800 hover:bg-green-950 shadow-xl transition-all duration-300"
+                            >
+                                <p className="text-slate-300 hover:text-white font-semibold flex-1">
+                                    {clicked
+                                        ? <SubmitSpinner />
+                                        : "OK"}
+                                </p>
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                </div>
+            </motion.div>
+        </>
+    );
+}
+
+
