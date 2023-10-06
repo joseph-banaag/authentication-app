483459ea415fc069ff235f8075438dcf29df0b3c
joseph-banaag <josephrbanaag51@gmail.com>
2023-10-06 11:44:33 +0800

    saving some changes.

--- components/toggle/ThemeSwitcher.tsx
+++ components/toggle/ThemeSwitcher.tsx
@@ -42,19 +42,24 @@ const ThemeSwitcher = () => {
       <div
         onClick={toggleButton}
         className={`relative sm:w-11 sm:h-5 w-9 h-4 p-0 flex items-center cursor-pointer rounded-2xl bg-violet-900 ms-2.5 !ps-[-5px] 
-        ${clicked
-            ? "active"
+        ${client
+            ? theme === "light" || !theme
+              ? "active"
+              : ""
             : ""
+
           }
         `}
       >
-        {clicked
-          ? <ToggleLightStyle />
-          : <ToggleDarkStyle />
+        {client
+          ? theme === "light" || !theme
+            ? <ToggleLightStyle />
+            : <ToggleDarkStyle />
+          : ""
         }
 
         {client
-          ? storedThemeValue === "dark" || storedThemeValue === "" || storedThemeValue === null || storedThemeValue === "undefined"
+          ? storedThemeValue === "dark" || !storedThemeValue
             ? <div
               onClick={handleThemeToLight}
               className="slider sm:w-9 sm:h-6 w-7 h-5 absolute sm:left-[-20px] left-[-15px] flex justify-center items-center rounded-sm truncate !text-white text-sm capitalize bg-violet-500 rounded-l-md ">
