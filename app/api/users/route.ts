4010036866dbc5e4a4c3b7e1d79b121f5d72470f
joseph-banaag <josephrbanaag51@gmail.com>
2023-10-12 00:17:11 +0800

    saving some changes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -13,19 +13,13 @@ export async function POST(request: Request) {
     const db = client.db("active_users");
     const collection = db.collection("user_information");
 
-    // const newDoc = {
-    //   email: `${email_acc}`,
-    //   username: `${user_name}`,
-    //   password: `${password}`,
-    //   created_on: `${created_on}`,
-    // };
+    const newDoc = {
+      email: `${email_acc}`,
+      username: `${user_name}`,
+      password: `${password}`,
+      created_on: `${created_on}`,
+    };
 
-     const newDoc = {
-       email: "testUser100@email.com",
-       username: "testUser102",
-       password: "testUser102!",
-       created_on: "Oct 11, 2023",
-     };
     const AddAcc = await collection.insertOne(newDoc);
 
     console.log("Successfully added a new user");
@@ -34,6 +28,9 @@ export async function POST(request: Request) {
     throw new Error(
       `There was a problem creating a new document. Error: ${error}`
     );
+  } finally {
+    await client.close();
+    console.log("The process is now completed. Database connection is closed.");
   }
 }
 
@@ -46,7 +43,7 @@ export async function GET(request: Request) {
 
     const toFind = {};
 
-    const createdUsers = await collection.find(toFind).toArray()
+    const createdUsers = await collection.find(toFind).toArray();
 
     console.log("List of the documents will be found through PostMan");
     return Response.json(createdUsers);
@@ -55,6 +52,9 @@ export async function GET(request: Request) {
     throw new Error(
       `There was a problem getting the information from the database. Error: ${error}`
     );
+  } finally {
+    await client.close();
+    console.log("The process is now completed. Database connection is closed.");
   }
 }
 
@@ -77,13 +77,16 @@ export async function PUT(request: Request) {
       $set: newData,
     });
 
-    console.log("Successfully updated an account")
+    console.log("Successfully updated an account");
     return Response.json(updateResult);
     // return new NextResponse(JSON.stringify(updateResult));
   } catch (error) {
     throw new Error(
       `There was a problem updating the document. Error: ${error}`
     );
+  } finally {
+    await client.close();
+    console.log("The process is now completed. Database connection is closed.");
   }
 }
 
@@ -107,10 +110,8 @@ export async function DELETE(request: Request) {
     throw new Error(
       `There was a problem deleting the document. Error: ${error}`
     );
+  } finally {
+    await client.close();
+    console.log("The process is now completed. Database connection is closed.");
   }
 }
-
-// finally {
-//     await client.close();
-//     console.log("The process is now completed. Database connection is closed.");
-//   }
