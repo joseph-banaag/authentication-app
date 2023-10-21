a1104f0566778fcb9413073cbcabcbbcbb360ef7
joseph-banaag <josephrbanaag51@gmail.com>
2023-10-21 16:52:01 +0800

    added a notfound page from the root if the user tried to access an unexisting page

---
+++ app/notFound/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { bgIllustration } from "@/components/constants";
+import { Image } from "@nextui-org/react";
+import Link from "next/link";
+
+export default function NotFound({
+  error,
+}: {
+  error: Error & { digest?: string };
+}): React.JSX.Element | null {
+  const [isClient, setIsClient] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.error(error);
+    setIsClient(true);
+  }, [error]);
+  return (
+    <>
+      <div className="text-foreground/80 flex flex-1 w-full h-screen justify-center items-center gap-8 flex-col">
+        <h2 className="text-4xl text-foreground/80 drop-shadow-xl font-bold">
+          {isClient ? "Something went wrong!" : ""}
+        </h2>
+        <Image
+          src={bgIllustration.errorNotFound.src}
+          alt={bgIllustration.errorNotFound.name}
+          width={600}
+          height={600}
+          style={{
+            objectFit: "cover",
+          }}
+          className="fade-in-bg"
+        />
+        <Link
+          href="/"
+          className="bg-[#661fe0] px-5 py-2 rounded-2xl text-xl font-semibold hover:scale-105 hover:bg-[hsl(262,76%,30%)] transition-all duration-300"
+        >
+          Home
+        </Link>
+      </div>
+    </>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,30 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+
+export const runtime = "experimental-edge";
 
 const unprotectedRoutes = ["/", "/sign-in", "/sign-up"];
+const protectedRoutes = ["/dashboard", "/settings", "/profile", "/security"];
+const pathPattern =
+  /^\/(?!$|dashboard$|profile$|settings$|security$|sign-in$|sign-up$)[a-zA-Z0-9_-]+\/?$/;
 
 export function middleware(request: NextRequest) {
   const usernameCookie = request.cookies.get("cookieName");
   const cookieValue = usernameCookie?.value;
 
+  if (pathPattern.test(request.nextUrl.pathname) === true) {
+    return NextResponse.rewrite(new URL("/notFound", request.url));
+  }
+
   if (cookieValue && unprotectedRoutes.includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  if (request.nextUrl.pathname.startsWith("/dashboard")) {
-    if (!cookieValue) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+  if (!cookieValue && protectedRoutes.includes(request.nextUrl.pathname)) {
+    console.log("no user");
+    return NextResponse.redirect(new URL("/", request.url));
   }
-  if (request.nextUrl.pathname.startsWith("/settings")) {
-    if (!cookieValue) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
-  if (request.nextUrl.pathname.startsWith("/profile")) {
-    if (!cookieValue) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
-  if (request.nextUrl.pathname.startsWith("/security")) {
-    if (!cookieValue) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
+
   return NextResponse.next();
 }
 
